6144857a54a45b7a01f4dcda2d0c47bc470f91c1
Ian Qin <toqinyan@gmail.com>
2020-09-22 22:29:48 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def hello(name, color):
         click.echo("Thor, you are always red.")
         click.echo(click.style(f"Hello {name}!", fg="red"))
     else:
-        click.echo(click.style(f"Hello World! {name}", fg=color))
+        click.echo(click.style(f"Hello {name}!", fg="color"))
 
 if __name__ == "__main__":
     hello()
